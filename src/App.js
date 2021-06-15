8f7b35cc53ca9191d56389f950eb70d313005fff
Mayen Akpan <neroxma@yahoo.com>
2021-06-15 14:08:17 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+import React, { Component } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
