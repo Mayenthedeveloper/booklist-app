f59343408b8724a1b35b8248d57f73dc9ec177af
Mayen Akpan <neroxma@yahoo.com>
2021-06-15 14:22:24 -0700

    Added form Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import "./App.css";
 import { v4 as uuidv4 } from "uuid";
-import React, { Component } from "react";
+import React, { useState } from "react";
+import Form from "./components/Form";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [isbn, setIsbn] = useState("");
+  const [currentBookId, setCurrentBookId] = useState(null);
+  const [books, setBooks] = useState([
+    {
+      bookTitle: title,
+      bookAuthor: author,
+      bookIsbn: isbn,
+      bookId: uuidv4(),
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome</h1>
-      </header>
+      <div className="container">
+        <Form />
+      </div>
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+function Form() {
+  return (
+    <form
+      action="
+       "
+    >
+      <label htmlFor="title">Title</label>
+      <input type="text" />
+      <label htmlFor="author">Author</label>
+      <input type="text" />
+      <label htmlFor="isbn">ISBN</label>
+      <input type="text" />
+    </form>
+  );
+}
+
+export default Form;
